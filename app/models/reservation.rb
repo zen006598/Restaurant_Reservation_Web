cacec5e09d61bb304ab34a371c469cce3a1cecaf
Jeff Chen <zen00326598@gmail.com>
2023-04-09 20:39:19 +0800

    redis set the unavailable_time

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -11,11 +11,17 @@ class Reservation < ApplicationRecord
   validates :adult_quantity, presence: true, numericality: { greater_than: 0 }
   validates :child_quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates_datetime :arrival_time
-  validate :verify_arrival_time_in_business_day, :verify_arrival_time_in_business_time
+  validate :verify_arrival_time_in_business_time
 
   enum :gender, {male: 1, female: 2, other: 3}, default: :male
 
   scope :on_or_after_now, -> { where('arrival_time >= ?', Time.current)}
+  scope :all_reservations, -> (restaurant_id){where('restaurant_id = ?', restaurant_id)}
+  scope :except_canceled, -> {where.not(state: 'canceled')}
+  scope :which_table_type, -> (table_type) {where('table_type = ?', table_type)}
+  scope :when_arrival_time, -> (arrival_time) {where('arrival_time = ?', arrival_time)}
+
+  after_save :get_unavailable_time
 
   private
 
@@ -23,20 +29,28 @@ class Reservation < ApplicationRecord
     day_of_week = arrival_time.wday
     time_module = restaurant.time_modules.in_which_time_module(day_of_week)
     interval_time = restaurant.interval_time
-    time_module.business_times
-    business_times = BusinessTimeCounting.new(time_module, interval_time, arrival_time).reservable_time
 
-    if business_times.exclude?(arrival_time.strftime('%R').to_time.to_i)
-      return errors.add(:arrival_time, 'The arrival time must in the business time.') 
+    business_times = BusinessTimeCounting.new(time_module, interval_time, arrival_time).reservable_time
+    if business_times.exclude?(arrival_time.to_i)
+      return errors.add(:arrival_time, 'The arrival time must in the business time.')
     end
   end
 
-  def verify_arrival_time_in_business_day
-    enable_day = ReservationDate.new(restaurant.off_days, 
-                                    restaurant.time_modules,
-                                    restaurant.period_of_reservation).enable_dates
-    arrival_day = arrival_time.to_date
-    return errors.add(:arrival_time, 
-                      'The arrival time must in the business day.') if enable_day.exclude?(arrival_day)
+  def get_unavailable_time
+    table_type = Reservation.table_types[self.table_type]
+    reservations = Reservation.all_reservations(restaurant_id).except_canceled.which_table_type(table_type).when_arrival_time(arrival_time)
+
+    table_type_maximun = restaurant.seats.table_count(table_type)
+    reservations_number = reservations.size
+
+    if table_type_maximun <= reservations_number
+      sha_id = Digest::SHA1.hexdigest(restaurant_id.to_s)
+      key = "unavailable_time:#{sha_id}:#{arrival_time.strftime('%Y%m%d')}"
+      expire_time = (arrival_time.to_date.to_time + 1.days).to_i - Time.current.to_i
+      # Grouping the unavailable_time in the same day
+      # When the date for the unavailable time has passed, it will expire.
+      $redis.expire(key, expire_time, nx: true)
+      $redis.sadd(key, arrival_time)
+    end
   end
 end
--- app/models/seat.rb
+++ app/models/seat.rb
@@ -11,6 +11,9 @@ class Seat < ApplicationRecord
   enum :state, { empty: 0, occupy: 1 }, default: :empty
 
   scope :table_count, -> (table_type) { where(table_type: table_type).count }
+  scope :regular_table_count, -> { where(table_type: 0).count }
+  scope :private_room_count, -> { where(table_type: 1).count }
+  scope :counter_seat_count, -> { where(table_type: 2).count }
   
   def self.table_type_sum(table_type)
     table_count(table_type)
